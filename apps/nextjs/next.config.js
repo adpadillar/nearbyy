0d36201594e07f502616ac205e226d3d02db846b
Axel Padilla <adpadillar25@gmail.com>
2023-12-19 14:10:57 -0700

    installed db package

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -5,7 +5,7 @@ import "./src/env.js";
 const config = {
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@nearbyy/api"],
+  transpilePackages: ["@nearbyy/api", "@nearbyy/db"],
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
