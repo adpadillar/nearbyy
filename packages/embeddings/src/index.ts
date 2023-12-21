eea84e484330125546a9374714793d43acb71101
Axel Padilla <adpadillar25@gmail.com>
2023-12-20 19:19:16 -0700

    single embeddings

--- packages/embeddings/src/index.ts
+++ packages/embeddings/src/index.ts
@@ -1,1 +1,14 @@
-export const name = "embeddings";
+import OpenAI from "openai";
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_KEY,
+});
+
+export const getSingleEmbedding = async (toEmbed: string) => {
+  const embeddings = await client.embeddings.create({
+    model: "text-embedding-ada-002",
+    input: [toEmbed],
+  });
+
+  return embeddings.data[0]?.embedding;
+};
