1fd933f80aa63fd0117140bb65255ba9aa82af0b
Axel Padilla <adpadillar25@gmail.com>
2023-12-28 18:44:56 -0700

    more documentation

--- apps/nextjs/src/app/api/files/route.ts
+++ apps/nextjs/src/app/api/files/route.ts
@@ -4,8 +4,13 @@ import { withKeyAuth } from "@nearbyy/auth";
 import { db } from "@nearbyy/db";
 import { getSingleEmbedding } from "@nearbyy/embeddings";
 
-import { syncFileBodySchema } from "./types";
-
+/**
+ * POST `/api/files`
+ *
+ * This endpoint takes a URL to a file, extracts the text from it, and stores it
+ * in the vector database. It will return a 200 if the file was successfully stored
+ * or a 415 if the file type is not supported
+ */
 export const POST = withKeyAuth(
   async ({ body, projectid }) => {
     // We download the file from the URL
@@ -36,9 +41,19 @@ export const POST = withKeyAuth(
     // If the file is not supported
     return new Response("Unsupported file type", { status: 415 });
   },
-  { bodySchema: syncFileBodySchema },
+  {
+    bodyValidator: z.object({
+      fileUrl: z.string().url(),
+    }),
+  },
 );
 
+/**
+ * GET `/api/files`
+ *
+ * This endpoint takes a query and returns the most similar files to that query
+ * in the vector database
+ */
 export const GET = withKeyAuth(
   async ({ params }) => {
     // Get the embedding of the query
@@ -68,9 +83,9 @@ export const GET = withKeyAuth(
     );
   },
   {
-    paramsSchema: z.object({
+    paramsValidator: z.object({
       query: z.string(),
-      limit: z.coerce.number().default(10),
+      limit: z.coerce.number().lte(100).gt(0).int().default(10),
     }),
   },
 );
--- apps/nextjs/src/app/api/files/types.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-
-export const syncFileBodySchema = z.object({
-  fileUrl: z.string().url(),
-});
