12d52c91afa598bce1984e77fbf47b127c1feda8
Axel Padilla <adpadillar25@gmail.com>
2024-01-07 15:04:05 -0700

    validators inferred from schema

--- packages/db/src/lib/client.ts
+++ packages/db/src/lib/client.ts
@@ -1,11 +1,11 @@
-import type { z } from "zod";
 import { neon, neonConfig } from "@neondatabase/serverless";
 import { sql } from "drizzle-orm";
 import * as helpers from "drizzle-orm";
 import { drizzle as neonDrizzle } from "drizzle-orm/neon-http";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
 
 import * as schema from "../schema";
-import * as validators from "../validators";
 
 /**
  *
@@ -20,61 +20,35 @@ export function buildDbClient({ directUrl }: { directUrl: string }) {
 
 const client = buildDbClient({ directUrl: process.env.NEON_DIRECT_URL! });
 
-/**
- * The database client.
- * Includes the drizzle client, the custom vector client, the schema, and the validators.
- *
- * @field drizzle The Drizzle client.
- * @field vector The vector client.
- * @field schema The schema of the database.
- * @field validators The validators for the database.
- */
 export const db = {
-  /**
-   * The Drizzle client. Result of `buildDbClient`.
-   */
   drizzle: client,
-  /**
-   * The schema of the database. Includes all the tables and fields.
-   * Each field HAS to have a validator, defined in `validators`.
-   */
   schema,
-  /**
-   * The validators for the database. Includes all the tables and fields.
-   * Each validator CORRESPONDS to a table in `schema`.
-   */
-  validators,
-  /**
-   * All the drizzle helpers, like eq, sql, etc. Basically all the functions
-   * exported from drizzle-orm.
-   */
+  validator: <T extends keyof typeof schema>(
+    tablename: T,
+    schematype: "create" | "insert" = "create",
+  ) => {
+    const table = schema[tablename];
+    const validator =
+      schematype === "create"
+        ? createSelectSchema(table)
+        : createInsertSchema(table);
+
+    const validatorWithDistance = validator.merge(
+      z.object({ distance: z.number().optional() }),
+    );
+
+    return validatorWithDistance;
+  },
   helpers,
-  /**
-   * The vector client. Allows you to query the database for similar embeddings.
-   * This is a custom client, running custom pg-vector queries through Drizzle.
-   */
   vector: {
-    /**
-     * Allows you to query the database for similar embeddings.
-     *
-     * @param tablename the name of the table to query
-     * @param fieldname the field in the table to query
-     * @param embedding the embedding to query against
-     * @param limit the limit of results to return
-     * @returns the results of the query as an array of objects
-     */
-    similarity: async <
-      T extends keyof typeof schema,
-      U extends z.infer<(typeof validators)[T]>,
-    >(
+    similarity: async <T extends keyof typeof schema>(
       tablename: T,
       fieldname: keyof (typeof schema)[T],
       embedding: number[],
       limit = 10,
-    ): Promise<U[]> => {
+    ) => {
       const table = schema[tablename];
       const field = table[fieldname];
-      const validator = validators[tablename];
 
       const embeddings = embedding.toString();
 
@@ -85,9 +59,9 @@ export const db = {
       const res = await client.execute(statement);
 
       // parse all the rows
+      const validator = db.validator(tablename);
       const wrappedValidator = validator.array();
-
-      const parsed = wrappedValidator.parse(res.rows) as U[];
+      const parsed = wrappedValidator.parse(res.rows);
 
       return parsed;
     },
