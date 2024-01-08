fa0fcd5041bef3455e222a9b16707667d86b1adb
Axel Padilla <adpadillar25@gmail.com>
2024-01-08 15:25:00 -0700

    background

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,6 +1,3 @@
-import Link from "next/link";
-
-import BackgroundPattern from "~/components/BackgroundPattern";
 import Navigation from "~/components/Navigation";
 import SideBlob from "~/components/SideBlob";
 
@@ -11,29 +8,15 @@ export default async function HomePage() {
   return (
     <main>
       <Navigation />
-      <BackgroundPattern className="absolute -top-full w-screen" />
-      <div className="flex h-screen flex-col items-center justify-center">
-        <div className="absolute z-40 flex flex-col items-center justify-center space-y-12 pb-36">
-          <div className="flex flex-col items-center justify-center space-y-8">
-            <h1 className="max-w-4xl text-center text-7xl font-bold text-white">
-              AI Context Made Simple: Upload, Search, Access
-            </h1>
-
-            <p className="text-xl font-light text-[#F2F6FA] opacity-80">
-              Extend your AI model capabilities with a simple API call
-            </p>
-          </div>
-
-          <Link
-            href="/dashboard"
-            className="rounded-full border bg-black px-12 py-4 text-center text-xl text-white transition-all hover:scale-105 active:scale-95"
-          >
-            join the beta now!
-          </Link>
-        </div>
-
-        <SideBlob className="absolute -left-32 -top-[64rem] z-0 -translate-x-[20%] scale-50 opacity-70" />
-        <SideBlob className="absolute -top-[64rem] right-32 z-0 translate-x-[35%] -scale-x-50 scale-y-50 opacity-70" />
+      <div
+        className="flex min-h-screen w-screen flex-col items-center justify-center"
+        style={{
+          backgroundImage: `url(/brand/pattern.svg)`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <SideBlob className="absolute -left-72 -top-1/2" />
       </div>
     </main>
   );
--- apps/nextjs/src/components/SideBlob.tsx
+++ apps/nextjs/src/components/SideBlob.tsx
@@ -6,19 +6,75 @@ interface SideBlobProps extends React.SVGProps<SVGSVGElement> {
   children?: React.ReactNode;
 }
 
-export const Box = (): JSX.Element => {
-  return (
-    <div className="h-[3647px] w-[3222px]">
-      <img
-        className="fixed left-0 top-0 h-[2678px] w-[1900px]"
-        alt="Group"
-        src="group-12.png"
-      />
-    </div>
-  );
-};
-
 const SideBlob: React.FC<SideBlobProps> = (props) => {
+  return (
+    <svg
+      width="1943"
+      height="2678"
+      viewBox="0 0 1943 2678"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g style={{ mixBlendMode: "color-dodge" }}>
+        <path
+          d="M-1322 2282.04L-545.133 -969L1111.13 -573.219L334.267 2677.82L-1322 2282.04Z"
+          fill="url(#paint0_radial_9_6)"
+          fillOpacity="0.78"
+        />
+      </g>
+      <g style={{ mixBlendMode: "color-dodge" }}>
+        <path
+          d="M-800.594 8.22119L1707.34 656.979L1376.82 1934.66L-1131.11 1285.91L-800.594 8.22119Z"
+          fill="url(#paint1_radial_9_6)"
+          fillOpacity="0.78"
+        />
+      </g>
+      <g style={{ mixBlendMode: "color-dodge" }}>
+        <path
+          d="M-565.337 69.0779L1942.59 717.836L1612.08 1995.52L-895.852 1346.76L-565.337 69.0779Z"
+          fill="url(#paint2_radial_9_6)"
+          fillOpacity="0.78"
+        />
+      </g>
+      <defs>
+        <radialGradient
+          id="paint0_radial_9_6"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(-105 739) rotate(6.75435) scale(977.786 1919.27)"
+        >
+          <stop stopColor="#FF4B00" />
+          <stop offset="1" stopColor="#1C1C1C" stopOpacity="0" />
+        </radialGradient>
+        <radialGradient
+          id="paint1_radial_9_6"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(313.262 948.632) rotate(104.503) scale(659.871 1295.24)"
+        >
+          <stop stopColor="#FF4B00" />
+          <stop offset="1" stopColor="#1C1C1C" stopOpacity="0.08" />
+        </radialGradient>
+        <radialGradient
+          id="paint2_radial_9_6"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(548.518 1009.49) rotate(104.503) scale(659.871 1295.24)"
+        >
+          <stop stopColor="#00D3FF" />
+          <stop offset="1" stopColor="#1C1C1C00" stopOpacity="0.08" />
+        </radialGradient>
+      </defs>
+    </svg>
+  );
+
   return (
     <svg
       width="1599"
