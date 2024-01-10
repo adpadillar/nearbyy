8dbaec109bd59c8ac670851d8bbc9ecff6e9dd98
NicoleArana <nicole.arana.sasj@gmail.com>
2024-01-10 00:19:33 -0600

    center text using flex

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -8,14 +8,13 @@ export default async function HomePage() {
   return (
     <main>
       <Navigation />
-      <div className="pt-20">
-        <h1 className="text-center text-2xl text-white">
+      <div className="flex min-h-[80vh] items-center justify-center">
+        <h1 className="max-w-4xl text-center text-6xl font-extrabold leading-relaxed text-white">
           AI Context Made Simple: Upload, Search, Access
         </h1>
-
-        <SideBlob side="right" className="absolute right-0" />
-        <SideBlob side="left" className="absolute -bottom-40 left-0" />
       </div>
+      <SideBlob side="right" className="absolute right-0 top-0" />
+      <SideBlob side="left" className="absolute -bottom-40 left-0" />
     </main>
   );
 }
