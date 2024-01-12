13c0851d28e010da8b8b5ca3ef7f723136fcca2d
NicoleArana <nicole.arana.sasj@gmail.com>
2024-01-11 23:18:57 -0600

    more updates

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { GeistMono } from "geist/font/mono";
 
@@ -11,6 +12,13 @@ export default async function HomePage() {
   return (
     <main>
       <Navigation />
+      <Image
+        alt="background_image"
+        src="/brand/background_pattern.svg"
+        width={1407}
+        height={883}
+        className="pointer-events-none absolute right-0 top-0"
+      />
       <div className="flex min-h-[80vh] flex-col items-center justify-center space-y-8 pt-44">
         <h1 className="max-w-4xl text-center text-[65px] font-extrabold leading-[78px] -tracking-[3px] text-white">
           AI Context Made Simple: Upload, Search, Access
@@ -21,12 +29,12 @@ export default async function HomePage() {
         <div className="flex space-x-4 pt-8">
           <Link
             href="/dashboard"
-            className="flex items-center justify-center rounded-[28px] border border-white bg-black/[0.44] px-10 py-4 text-xl font-extralight text-white"
+            className="flex items-center justify-center rounded-[28px] border-2 border-black bg-black/[0.44] px-10 py-4 text-xl font-extralight text-white"
           >
             get started!
           </Link>
           <pre
-            className={`${GeistMono.className} flex items-center justify-center rounded-[18px] border border-white bg-black/[0.44] px-10 py-4 font-mono text-lg  text-white`}
+            className={`${GeistMono.className} flex items-center justify-center rounded-[18px] border-2 border-black bg-black/[0.44] px-10 py-4 text-lg  text-white`}
           >
             npm install @nearbyy/core
           </pre>
