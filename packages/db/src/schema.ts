1e219ee8f0ba621558612543eedc2f6ec5ec1bfe
NicoleArana <nicole.arana.sasj@gmail.com>
2024-01-21 15:27:38 -0600

    created user and email tables, migrated them too

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,4 +1,11 @@
-import { doublePrecision, pgTable, serial, text } from "drizzle-orm/pg-core";
+import {
+  bigint,
+  boolean,
+  doublePrecision,
+  pgTable,
+  serial,
+  text,
+} from "drizzle-orm/pg-core";
 
 export const files = pgTable("files", {
   id: serial("id").notNull().primaryKey(),
@@ -15,4 +22,24 @@ export const keys = pgTable("keys", {
   salt: text("salt").notNull(),
   projectid: text("projectid").notNull(),
   userid: text("userid").notNull(),
+});
+
+export const users = pgTable("users", {
+  id: text("id").primaryKey(),
+  imageUrl: text("imageUrl").notNull(),
+  profileImageUrl: text("profileImageUrl").notNull(),
+  firstName: text("firstName").notNull(),
+  lastName: text("lastName").notNull(),
+  fullName: text("fullName").notNull(),
+  createdAt: bigint("createdAt", { mode: "bigint" }),
+  updatedAt: bigint("updatedAt", { mode: "bigint" }),
+  twoFactorEnabled: boolean("twoFactorEnabled").notNull(),
+  primaryEmailAddressId: text("primaryEmailAddressId").notNull(),
+});
+
+export const emails = pgTable("emails", {
+  id: text("id").primaryKey(),
+  emailAddress: text("emailAddress").notNull(),
+  userId: text("userId").notNull(),
+  isVerified: boolean("isVerified").notNull(),
 });
