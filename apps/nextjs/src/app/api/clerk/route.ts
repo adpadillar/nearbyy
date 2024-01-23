6bd3d6a5df5bf0ffb1128de1acc400f198e08c32
NicoleArana <nicole.arana.sasj@gmail.com>
2024-01-22 23:55:58 -0600

    added code to handle user deleting email alredy in db

--- apps/nextjs/src/app/api/clerk/route.ts
+++ apps/nextjs/src/app/api/clerk/route.ts
@@ -11,7 +11,7 @@ export const POST = async (req: NextRequest) => {
         await db.drizzle.insert(db.schema.users).values({
           firstName: evt.data.first_name,
           lastName: evt.data.last_name,
-          fullName: evt.data.first_name + " " + evt.data.first_name,
+          fullName: evt.data.first_name + " " + evt.data.last_name,
           id: evt.data.id,
           imageUrl: evt.data.image_url,
           primaryEmailAddressId: evt.data.primary_email_address_id,
@@ -42,38 +42,60 @@ export const POST = async (req: NextRequest) => {
         }
       }
       break;
-    case "user.updated": {
-      await db.drizzle.update(db.schema.users).set({
-        firstName: evt.data.first_name,
-        lastName: evt.data.last_name,
-        fullName: evt.data.first_name + " " + evt.data.first_name,
-        id: evt.data.id,
-        imageUrl: evt.data.image_url,
-        primaryEmailAddressId: evt.data.primary_email_address_id,
-        twoFactorEnabled: evt.data.two_factor_enabled,
-        createdAt: evt.data.created_at,
-        updatedAt: evt.data.updated_at,
-      });
+    case "user.updated":
+      {
+        await db.drizzle.update(db.schema.users).set({
+          firstName: evt.data.first_name,
+          lastName: evt.data.last_name,
+          fullName: evt.data.first_name + " " + evt.data.first_name,
+          id: evt.data.id,
+          imageUrl: evt.data.image_url,
+          primaryEmailAddressId: evt.data.primary_email_address_id,
+          twoFactorEnabled: evt.data.two_factor_enabled,
+          createdAt: evt.data.created_at,
+          updatedAt: evt.data.updated_at,
+        });
 
-      for (const i of evt.data.email_addresses) {
-        await db.drizzle
-          .insert(db.schema.emails)
-          .values({
-            emailAddress: i.email_address,
-            id: i.id,
-            isVerified: i.verification?.status == "verified",
-            userId: evt.data.id,
-          })
-          .onConflictDoUpdate({
-            target: db.schema.emails.id,
-            set: {
-              emailAddress: i.email_address,
-              id: i.id,
-              isVerified: i.verification?.status == "verified",
-              userId: evt.data.id,
-            },
-          });
+        const emailsDb = db.drizzle
+          .select({ emailAdress: db.schema.emails.emailAddress })
+          .from(db.schema.emails)
+          .where(db.helpers.eq(db.schema.emails.userId, evt.data.id));
+        const emailsClerk = evt.data.email_addresses.map(
+          (element) => element.email_address,
+        );
+
+        for (const i of await emailsDb) {
+          if (!emailsClerk.includes(i.emailAdress)) {
+            await db.drizzle
+              .delete(db.schema.emails)
+              .where(
+                db.helpers.eq(db.schema.emails.emailAddress, i.emailAdress),
+              );
+          }
+
+          for (const i of evt.data.email_addresses) {
+            await db.drizzle
+              .insert(db.schema.emails)
+              .values({
+                emailAddress: i.email_address,
+                id: i.id,
+                isVerified: i.verification?.status == "verified",
+                userId: evt.data.id,
+              })
+              .onConflictDoUpdate({
+                target: db.schema.emails.id,
+                set: {
+                  emailAddress: i.email_address,
+                  id: i.id,
+                  isVerified: i.verification?.status == "verified",
+                  userId: evt.data.id,
+                },
+              });
+          }
+        }
       }
-    }
+      return new Response("all good", {
+        status: 200,
+      });
   }
 };
