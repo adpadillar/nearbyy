7eaa30852f0c3021c13262a4cdec98a454814630
NicoleArana <nicole.arana.sasj@gmail.com>
2024-01-23 23:34:15 -0600

    finished background opts and added code block

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -3,30 +3,30 @@ import Link from "next/link";
 import { GeistMono } from "geist/font/mono";
 
 import Navigation from "~/components/Navigation";
+import Shiki from "~/components/Shiki.server";
 import SideBlob from "~/components/SideBlob";
 
-export const runtime = "edge";
-export const preferredRegion = "iad1";
-
 export default async function HomePage() {
+  const res = await fetch("http://localhost:3000/example/example-home.ts");
+  const text = (await res.text()) as string;
   return (
     <main>
       <Navigation />
       <Image
         alt="background_image"
         src="/brand/background_pattern.svg"
-        width={1407}
-        height={883}
-        className="pointer-events-none absolute right-0 top-0"
+        width={1446}
+        height={943}
+        className="pointer-events-none absolute right-[17px] top-[5.5rem] opacity-50"
       />
-      <div className="flex min-h-[80vh] flex-col items-center justify-center space-y-8 pt-44">
+      <div className="   flex min-h-[80vh] flex-col items-center justify-center space-y-9 pt-56">
         <h1 className="max-w-4xl text-center text-[65px] font-extrabold leading-[78px] -tracking-[3px] text-white">
           AI Context Made Simple: Upload, Search, Access
         </h1>
         <h2 className="text-center text-2xl font-thin text-white">
           Extend your AI model capabilities with a simple API call
         </h2>
-        <div className="flex space-x-4 pt-8">
+        <div className=" relative z-10 flex space-x-4 pt-6">
           <Link
             href="/dashboard"
             className="flex items-center justify-center rounded-[28px] border-2 border-black bg-black/[0.44] px-10 py-4 text-xl font-extralight text-white"
@@ -39,15 +39,39 @@ export default async function HomePage() {
             npm install @nearbyy/core
           </pre>
         </div>
+        <div className="pt-8">
+          <div className=" relative z-10 rounded-md bg-gradient-to-br from-red-600 to-cyan-500 p-[0.1rem]  shadow-2xl">
+            <Shiki
+              code={text}
+              lang="ts"
+              theme="css-variables"
+              className=" w-[40rem] rounded-md p-8"
+            ></Shiki>
+          </div>
+        </div>
       </div>
+
       <SideBlob
-        side="right"
+        side="right_1"
         className="pointer-events-none absolute right-0 top-0"
       />
       <SideBlob
-        side="left"
+        side="left_1"
         className="pointer-events-none absolute -bottom-40 left-0"
       />
+      {/* <SideBlob side="right_2" className="pointer-events-none absolute top-0" />
+      <SideBlob
+        side="left_2"
+        className="pointer-events-none absolute right-0 top-0"
+      /> */}
+      {/* <SideBlob
+        side="right_3"
+        className="pointer-events-none absolute left-0 top-0"
+      />
+      <SideBlob
+        side="left_3"
+        className="pointer-events-none absolute right-0 top-0"
+      /> */}
     </main>
   );
 }
