3d6948974c6ec7874e83d6cedd86aca7bd7ac27d
Axel Padilla <adpadillar25@gmail.com>
2024-01-31 14:08:54 -0700

    installed command, dialog and popover components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,6 @@
 export * from "./components/ui/card";
 export * from "./components/ui/button";
 export * from "./components/ui/alert-dialog";
+export * from "./components/ui/command";
+export * from "./components/ui/dialog";
+export * from "./components/ui/popover";
