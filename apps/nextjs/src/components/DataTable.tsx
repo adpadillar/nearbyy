2f5598837d0627e5f1962f4778d52468b63a45af
Axel Padilla <adpadillar25@gmail.com>
2024-02-01 13:10:00 -0700

    added pagination to table

--- apps/nextjs/src/components/DataTable.tsx
+++ apps/nextjs/src/components/DataTable.tsx
@@ -4,10 +4,12 @@ import type { ColumnDef } from "@tanstack/react-table";
 import {
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
 import {
+  Button,
   Table,
   TableBody,
   TableCell,
@@ -29,6 +31,7 @@ export function DataTable<TData, TValue>({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   });
 
   return (
@@ -75,6 +78,25 @@ export function DataTable<TData, TValue>({
           )}
         </TableBody>
       </Table>
+      <div className="flex items-center justify-end space-x-2 px-4 py-4">
+        {/* Pagination */}
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
     </div>
   );
 }
