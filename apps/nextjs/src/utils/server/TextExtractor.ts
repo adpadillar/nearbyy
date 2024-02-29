401b6a5f37caf49a1008185e236d0ebe9764d6c6
Axel Padilla <adpadillar25@gmail.com>
2024-02-29 15:13:01 -0600

    PDF Support
    - Missing full support because PDFs can be too long for the text-ada-002 model
    - Once chunking is merged, this shouldn't be a problem

--- apps/nextjs/src/utils/server/TextExtractor.ts
+++ apps/nextjs/src/utils/server/TextExtractor.ts
@@ -1,3 +1,7 @@
+import { getDocument } from "pdfjs-dist";
+
+import "pdfjs-dist/build/pdf.worker.min";
+
 export class TextExtractor {
   buffer: ArrayBuffer;
   mimeType: string;
@@ -12,9 +16,35 @@ export class TextExtractor {
     return text;
   }
 
+  async extractFromPdf() {
+    const doc = await getDocument(this.buffer).promise;
+    const textArray: string[] = [];
+
+    for (let i = 0; i < doc.numPages; i++) {
+      const page = await doc.getPage(i + 1); // pages start at 1
+      const text = await page.getTextContent();
+      const finalText = text.items
+        .map((s) => {
+          const v = s as { str: string };
+          return v.str;
+        })
+        .join(" ");
+      textArray.push(finalText);
+    }
+
+    const text = textArray.join("\n");
+    console.log(text);
+
+    return text;
+  }
+
   async extract() {
     if (this.mimeType.startsWith("text/")) {
       return { text: this.extractFromText(), error: null } as const;
+    }
+
+    if (this.mimeType === "application/pdf") {
+      return { text: await this.extractFromPdf(), error: null } as const;
     }
 
     return { text: null, error: "Unsupported file type" } as const;
