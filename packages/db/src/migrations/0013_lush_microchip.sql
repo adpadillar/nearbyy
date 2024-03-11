0ec8f7a13b034e3a83256a9aa3a5af0bc14dc0ce
NicoleArana <nicole.arana.sasj@gmail.com>
2024-03-11 00:12:01 -0600

    back to normal

---
+++ packages/db/src/migrations/0013_lush_microchip.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "files" RENAME COLUMN "projectid" TO "projectId";--> statement-breakpoint
+ALTER TABLE "keys" RENAME COLUMN "projectid" TO "projectId";
---
+++ packages/db/src/migrations/0014_awesome_doctor_strange.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "files" RENAME COLUMN "projectId" TO "projectid";--> statement-breakpoint
+ALTER TABLE "keys" RENAME COLUMN "projectId" TO "projectid";
