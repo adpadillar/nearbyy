9516a250306c1c66d8b4e8d5d4ddb6eeb3f0106f
Axel Padilla <adpadillar25@gmail.com>
2024-03-11 14:20:30 -0600

    Revert "disable minify"
    This reverts commit 57b138cf660987a66e6466f46cebeb953d671331.

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -4,9 +4,7 @@ import "./src/env.js";
 import CopyPlugin from "copy-webpack-plugin";
 
 const dontBundlePdf2Json = new CopyPlugin({
-  patterns: [
-    { from: "../../node_modules/pdf2json/**/*", to: "node_modules/pdf2json" },
-  ],
+  patterns: ["node_modules/pdf2json/**/*"],
 });
 
 /** @type {import("next").NextConfig} */
@@ -33,14 +31,9 @@ const config = {
   experimental: {
     serverComponentsExternalPackages: ["pdf2json"],
   },
-  webpack: (config, { isServer, nextRuntime }) => {
-    if (isServer && nextRuntime === "nodejs") {
-      config.externals.push("pdf2json");
-      config.plugins.push(dontBundlePdf2Json);
-      config.optimization.minimize = false;
-    }
-
-    return config;
+  webpack: {
+    externals: ["pdf2json"],
+    plugins: [dontBundlePdf2Json],
   },
 };
 
