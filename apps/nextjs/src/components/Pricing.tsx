d3fd5d29f8384ac9ccc8c5cf55ea0445ec231d6c
NicoleArana <nicole.arana.sasj@gmail.com>
2024-03-30 17:06:29 -0600

    Almost Done

--- apps/nextjs/src/components/Pricing.tsx
+++ apps/nextjs/src/components/Pricing.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useTailwindBreakpoints } from "~/hooks/useTailwindBreakpoints";
 import PricingCard from "./PricingCard";
 
 interface PricingProps {
@@ -8,20 +9,27 @@ interface PricingProps {
 }
 
 const Pricing: React.FC<PricingProps> = ({ path }) => {
+  const breakpoint = useTailwindBreakpoints();
+
   return (
     <div className="z-20 flex max-w-[70rem] flex-col items-center space-y-4 ">
       {/* Title and subtitle */}
       <div className="flex flex-col items-center">
-        <h1 className="text-6xl font-bold text-white">Empower Your Growth,</h1>
-        <h1 className=" inline-block bg-gradient-to-r from-red-600 to-[#166AE8] bg-clip-text text-6xl  font-bold text-transparent">
+        <h1
+          style={{ textWrap: "balance" }}
+          className=" text-center text-5xl font-bold text-white sm:text-6xl"
+        >
+          Empower Your Growth,
+        </h1>
+        <h1 className="inline-block bg-gradient-to-r from-red-600 to-[#166AE8] bg-clip-text text-center text-5xl  font-bold text-transparent  sm:text-6xl ">
           explore pricing.
         </h1>
-        <h2 className="pb-10 pt-6 font-normal text-white text-opacity-70 ">
+        <h2 className="px-10 pb-8 pt-10 text-justify text-lg font-normal text-white text-opacity-70 sm:pb-10 sm:pt-6  ">
           Start building something great, minus the pricing puzzle
         </h2>
       </div>
 
-      <div className="grid grid-cols-3 gap-x-6">
+      <div className=" grid grid-cols-1 gap-y-8 px-8 sm:grid-cols-2  sm:gap-x-4 sm:px-4 lg:grid-cols-3 lg:gap-x-4 lg:px-4 xl:gap-x-6">
         {/* BOX 1 */}
         <PricingCard
           content={{
@@ -69,7 +77,9 @@ const Pricing: React.FC<PricingProps> = ({ path }) => {
             header: "Enterprise",
             headerStyle: "primary",
             subTitle:
-              "Is Pro not Pro enough? More files, more devs, and usage based pricing",
+              breakpoint === "lg"
+                ? "More files, more devs, and usage based pricing"
+                : "Is Pro not Pro enough? More files, more devs, and usage based pricing",
             price: "Custom*",
             path: path,
             linkText: "Schedule a call",
