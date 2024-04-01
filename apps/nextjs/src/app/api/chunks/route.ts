a187c43d94d956aa4a942772cc13972fc3a373d2
Axel Padilla <adpadillar25@gmail.com>
2024-04-01 00:15:14 -0600

    hopefully usage metrics and rate-limitinh (20k/mo limit)

--- apps/nextjs/src/app/api/chunks/route.ts
+++ apps/nextjs/src/app/api/chunks/route.ts
@@ -6,8 +6,53 @@ import {
 import { db } from "@nearbyy/db";
 import { getSingleEmbedding } from "@nearbyy/embeddings";
 
+const FREE_REQUEST_LIMIT = 20_000;
+const ONE_MONTH_MS = 30 * 24 * 60 * 60 * 1000;
+
 export const GET = withKeyAuth({
   handler: async ({ params, projectid }) => {
+    const mutationResult = await db.drizzle
+      .update(db.schema.projects)
+      .set({
+        queryCount: db.helpers.sql`${db.schema.projects.queryCount} + 1`,
+        runningQueryCount: db.helpers
+          .sql`${db.schema.projects.runningQueryCount} + 1`,
+      })
+      .where(db.helpers.eq(db.schema.projects.id, projectid))
+      .returning();
+
+    const currentProject = mutationResult[0]!;
+
+    // Check if the last reset was before the current date
+    const now = new Date();
+    const lastReset = currentProject.lastQuotaReset;
+    let wasReset = false;
+
+    // If the last reset was more than a month ago, reset the count
+    if (lastReset.getTime() < now.getTime() - ONE_MONTH_MS) {
+      wasReset = true;
+      await db.drizzle
+        .update(db.schema.projects)
+        .set({
+          runningQueryCount: 0,
+          lastQuotaReset: now,
+        })
+        .where(db.helpers.eq(db.schema.projects.id, projectid))
+        .execute();
+    }
+
+    // If the count was not reset and the count is over the limit
+    if (!wasReset && currentProject.queryCount > FREE_REQUEST_LIMIT + 1) {
+      return {
+        status: 429,
+        body: {
+          data: null,
+          success: false,
+          error: "You have exceeded the free request limit",
+        },
+      } as const;
+    }
+
     const { embedding, success } = await getSingleEmbedding(params.query);
 
     if (!success) {
