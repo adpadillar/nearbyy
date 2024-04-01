d98d3417b111bd835a0fd8f3e27d79904d9005be
Axel Padilla <adpadillar25@gmail.com>
2024-04-01 00:24:38 -0600

    abstracted query ratelimit

--- apps/nextjs/src/app/api/chunks/route.ts
+++ apps/nextjs/src/app/api/chunks/route.ts
@@ -1,4 +1,4 @@
-import { withKeyAuth } from "@nearbyy/auth";
+import { queryRatelimit, withKeyAuth } from "@nearbyy/auth";
 import {
   chunkEndpointGetParams,
   chunkEndpointGetResponse,
@@ -6,56 +6,26 @@ import {
 import { db } from "@nearbyy/db";
 import { getSingleEmbedding } from "@nearbyy/embeddings";
 
-const FREE_REQUEST_LIMIT = 20_000;
-const ONE_MONTH_MS = 30 * 24 * 60 * 60 * 1000;
-
 export const GET = withKeyAuth({
   handler: async ({ params, projectid }) => {
-    const mutationResult = await db.drizzle
-      .update(db.schema.projects)
-      .set({
-        queryCount: db.helpers.sql`${db.schema.projects.queryCount} + 1`,
-        runningQueryCount: db.helpers
-          .sql`${db.schema.projects.runningQueryCount} + 1`,
-      })
-      .where(db.helpers.eq(db.schema.projects.id, projectid))
-      .returning();
+    const { success: rateLimitSuccess } = await queryRatelimit(projectid);
 
-    const currentProject = mutationResult[0]!;
-
-    // Check if the last reset was before the current date
-    const now = new Date();
-    const lastReset = currentProject.lastQuotaReset;
-    let wasReset = false;
-
-    // If the last reset was more than a month ago, reset the count
-    if (lastReset.getTime() < now.getTime() - ONE_MONTH_MS) {
-      wasReset = true;
-      await db.drizzle
-        .update(db.schema.projects)
-        .set({
-          runningQueryCount: 0,
-          lastQuotaReset: now,
-        })
-        .where(db.helpers.eq(db.schema.projects.id, projectid))
-        .execute();
-    }
-
-    // If the count was not reset and the count is over the limit
-    if (!wasReset && currentProject.queryCount > FREE_REQUEST_LIMIT + 1) {
+    if (!rateLimitSuccess) {
       return {
         status: 429,
         body: {
           data: null,
           success: false,
-          error: "You have exceeded the free request limit",
+          error: "Rate limit exceeded",
         },
       } as const;
     }
 
-    const { embedding, success } = await getSingleEmbedding(params.query);
+    const { embedding, success: embeddingSuccess } = await getSingleEmbedding(
+      params.query,
+    );
 
-    if (!success) {
+    if (!embeddingSuccess) {
       return {
         status: 500,
         body: {
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,3 +1,4 @@
 export { withAuth } from "./next/withAuth";
 export { withKeyAuth } from "./next/withKeyAuth";
 export { validateKey, generateKey } from "./utils";
+export { queryRatelimit } from "./utils/queryRatelimit";
