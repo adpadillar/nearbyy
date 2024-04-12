a11ec28c4bd2d8d4306ec5147bc7ccc7b66e70f1
Axel Padilla <adpadillar25@gmail.com>
2024-04-12 17:35:57 -0600

    get Presigned url

---
+++ apps/nextjs/src/utils/server/getPresignedUrl.ts
@@ -0,0 +1,40 @@
+import { S3Client } from "@aws-sdk/client-s3";
+import { createPresignedPost } from "@aws-sdk/s3-presigned-post";
+
+import { env } from "~/env";
+
+const client = new S3Client({
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+interface GetPresignedUrlResponse {
+  url: string;
+  fields: Record<string, string>;
+}
+
+export async function getPresignedUrl(
+  fileId: string,
+  projectId: string,
+  contentType: string,
+): Promise<GetPresignedUrlResponse> {
+  const { url, fields } = await createPresignedPost(client, {
+    Bucket: "nearbyy-file-storage",
+    Key: fileId,
+    Conditions: [
+      ["content-length-range", 0, 10485760], // up to 10 MB
+      ["starts-with", "$Content-Type", contentType],
+    ],
+    Fields: {
+      "Content-Type": contentType,
+      "x-nearbyy-project-id": projectId,
+      "x-nearbyy-file-id": fileId,
+    },
+    Expires: 3600,
+  });
+
+  return { url, fields };
+}
