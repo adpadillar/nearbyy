3c92ec1a41954000c79f233e8004df84e2485ca1
Axel Padilla <adpadillar25@gmail.com>
2024-04-13 21:24:39 -0600

    Get presigned url endpoint

--- apps/nextjs/src/app/api/files/get-upload-url/route.ts
+++ apps/nextjs/src/app/api/files/get-upload-url/route.ts
@@ -10,24 +10,35 @@ export const GET = withKeyAuth({
   handler: async ({ params, projectid }) => {
     const fileId = crypto.randomUUID();
 
-    const { url, fields } = await getPresignedUrl(
-      fileId,
-      projectid,
-      params.contentType,
-    );
+    try {
+      const { url, fields } = await getPresignedUrl(
+        fileId,
+        projectid,
+        params.contentType,
+      );
 
-    return {
-      status: 200,
-      body: {
-        data: {
-          uploadUrl: url,
-          fileId: fileId,
-          fields: fields,
+      return {
+        status: 200,
+        body: {
+          data: {
+            uploadUrl: url,
+            fileId: fileId,
+            fields: fields,
+          },
+          error: null,
+          success: true,
         },
-        error: null,
-        success: true,
-      },
-    } as const;
+      } as const;
+    } catch {
+      return {
+        status: 500,
+        body: {
+          data: null,
+          error: "Failed to get upload URL",
+          success: false,
+        },
+      } as const;
+    }
   },
   schema: {
     params: getUploadUrlEndpointGetParams,
